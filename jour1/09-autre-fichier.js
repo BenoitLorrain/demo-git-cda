c8da99471bf997197d3f52cb1b5477d3441d54ce
Benoît Lorrain <benoitlorrain@gmail.com>
2023-03-27 17:07:40 +0200

    fin jour1

---
+++ jour1/09-autre-fichier.js
@@ -0,0 +1,15 @@
+const a = {
+    largeur : 20,
+    hauteur : 30,
+    unite : "cm"
+};
+
+function calcul(){
+    return `${a.largeur} x ${a.hauteur} = ${a.largeur * a.hauteur}`
+};
+
+//git branch -M menu-principal
+//git checkout master
+//git branch -M main
+//git checkout menu-principal
+//git branch -M menu
